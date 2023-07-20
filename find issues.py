8d06fd02f7514198f7ee120e03090296c18fbe48
Amanda Hogan <hogesonline@gmail.com>
2023-07-20 17:56:43 +1000

    Fixed a bunch of things including stripping trailing ints and added nonsense

--- find issues.py
+++ find issues.py
@@ -2,18 +2,30 @@ import pickle
 with open('final_lyrics_dump.pickle', 'rb') as handle:
     items = pickle.load(handle)
 
-bad = []
+def endwithnum(s):
+    if s[-1].isdigit():
+        return True
+    else:
+        return False
+
+def fix_suffix(s):
+    while s[-1].isdigit():
+        s = s[:-1]
+        return s
 
 count = 0
-with open('read_the_lyrics.txt', 'w') as out_file:
-    for item in items:
-        try:
-            out_file.write(str(count) + " - " +item['Song title']+':' + item['Artist']+'\n')
-            out_file.write(item['Lyrics']+ '\n\n')
-        except:
-            bad.append(str(count) + " - " +item['Song title']+':' + item['Artist']+'\n' + item['Lyrics']+ '\n\n')
-        count += 1
+with open("read_the_lyrics.txt", "w") as file:
 
-with open('bad_lyrics.txt', 'w', encoding='utf-8') as out_file:
-    for item in bad:
-        out_file.write(item)
+    for item in items:
+        file.write(str(count) +" " + item['Artist'] + " : " + item["Song title"] + "\n\n")
+        file.write(item['Lyrics'] + "\n\n")
+        count += 1
+    # if endwithnum(item['Lyrics']):
+    #     item['Lyrics'] = fix_suffix(item['Lyrics'])
+    #     print("fixed", count)
+    #     count += 1
+    
+
+# with open('final_lyrics_dump.pickle', 'wb') as out_file:
+#     pickle.dump(items, out_file, protocol=pickle.HIGHEST_PROTOCOL)
+    
